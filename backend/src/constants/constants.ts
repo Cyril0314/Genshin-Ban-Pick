c5bf2bb2572ae4dc58bdf40293b3df2784c245f6
DESKTOP-7RFCMOE\asdfg <asdfgh123q0314@gmail.com>
2025-09-29 13:41:07 +0800

    feat: adjust number of utility

--- backend/src/constants/constants.ts
+++ backend/src/constants/constants.ts
@@ -1,6 +1,6 @@
 // backend/src/constants/constants.ts
 
-export const numberOfUtility = 8
+export const numberOfUtility = 4
 export const numberOfPick = 32
 export const numberOfBan = 8
 export const totalRounds = 2
